b5dc8c5aebb872ed005e4373000bd954adbfc186
Marius Adrian Cucu <marius.cucu94@gmail.com>
2017-12-06 20:23:37 +0200

    Initial Commit

---
+++ Snapchat App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snapchat App
+//
+//  Created by Marius Adrian Cucu on 06/12/2017.
+//  Copyright © 2017 Marius Adrian Cucu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
